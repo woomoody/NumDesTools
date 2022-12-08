b5f2611349e06b63b9ec23109ce0bdf79819e295
cent shen <360151507@qq.com>
2022-12-08 18:42:02 +0800

    恢复

--- NumDesTools/DotaLegendBattle.cs
+++ NumDesTools/DotaLegendBattle.cs
@@ -29,34 +29,117 @@ namespace NumDesTools
             //初始化A、B两个阵营的
             dynamic app = ExcelDnaUtil.Application;
             Worksheet ws = app.Worksheets["战斗模拟"];
-            var groupAnum = Convert.ToInt32(ws.Range["D7"].Value);
-            var groupARowMax = Convert.ToInt32(ws.Range["C5"].Value);
-            var groupAColMax = Convert.ToInt32(ws.Range["C6"].Value);
-            var groupBnum = Convert.ToInt32(ws.Range["J7"].Value);
-            var groupBRowMax = Convert.ToInt32(ws.Range["K5"].Value);
-            var groupBColMax = Convert.ToInt32(ws.Range["K6"].Value);
-            //A、B两个阵营位置和人员确认
-            String[] groupA = new String[groupAnum];
-            String[] groupB = new String[groupBnum];
-            for (var i = 0;i <  groupAnum ;i++)
+            var groupARowMin = Convert.ToInt32(ws.Range["C9"].Value);
+            var groupAColMin = Convert.ToInt32(ws.Range["C10"].Value);
+            var groupARowMax = Convert.ToInt32(ws.Range["C11"].Value);
+            var groupAColMax = Convert.ToInt32(ws.Range["C12"].Value);
+            var groupARowNum = groupARowMax - groupARowMin + 1;
+            var groupAColNum = groupAColMax - groupAColMin + 1;
+            var groupBRowMin = Convert.ToInt32(ws.Range["C20"].Value);
+            var groupBColMin = Convert.ToInt32(ws.Range["C21"].Value);
+            var groupBRowMax = Convert.ToInt32(ws.Range["C22"].Value);
+            var groupBColMax = Convert.ToInt32(ws.Range["C23"].Value);
+            var groupBRowNum = groupBRowMax - groupBRowMin + 1;
+            var groupBColNum = groupBColMax - groupBColMin + 1;
+            //声明角色各属性所在列
+            var posRow = 1;//角色所在行
+            var posCol = 2;//角色所在列
+            var pos = 3;//角色在阵型中的位置
+            var name = 4;//角色名
+            var detailType = 5;//扩展类型
+            var type = 6;//大类型
+            var lvl = 7;//角色等级
+            var skillLv = 8;//技能等级
+            var atk = 9;//攻击力
+            var hp = 10;  //生命值
+            var def = 11;//防御力
+            var crit = 12;// 暴击率
+            var critMulti = 13;//暴击倍率 
+            var atkSpeed = 14;//攻速
+            var autoRatio = 15;//普攻占比
+            var skillCD = 16;//大招CD
+            var skillDamge = 17;//伤害倍率
+            var skillHealUseSelfAtk = 18;//治疗倍率/D
+            var skillHealUseSelfHp = 19;//治疗被驴/H
+            var skillHealUseAllHp = 20;//治疗倍率/A
+            Range rangeA = ws.Range[ws.Cells[groupARowMin, groupAColMin], ws.Cells[groupARowMax, groupAColMax]];
+            Array arrA = rangeA.Value2;
+            //过滤空位置A
+            List<int> roleA = new List<int>();
+            for (int i = 1; i < groupARowNum + 1; i++)
             {
-                groupA[i] = Convert.ToString(ws.Cells[10+i,5].Value);
+                if (arrA.GetValue(i, name) != null)
+                {
+                    roleA.Add(i);
+                }
+                else
+                {
+                    continue;
+                }
             }
-            for (var i = 0; i < groupBnum ; i++)
+            Range rangeB = ws.Range[ws.Cells[groupBRowMin, groupBColMin], ws.Cells[groupBRowMax, groupBColMax]];
+            Array arrB = rangeB.Value2;
+            //过滤空位置B
+            List<int> roleB = new List<int>();
+            for (int i = 1; i < groupBRowNum + 1; i++)
             {
-                groupB[i] = Convert.ToString( ws.Cells[21 + i, 5].Value);
+                if (arrB.GetValue(i, name) != null)
+                {
+                    roleB.Add(i);
+                }
+                else
+                {
+                    continue;
+                }
             }
-        }
-        //确定每个角色位置
-        public static void LocalRC(int roleNum, int rowMax, int colMax)
-        {
-            var roleR = ((roleNum + rowMax - 1) % rowMax) + 1;
-            var roleC = Math.Floor((double)((roleNum - 1) / colMax)) + 1;
+            //给A选择目标
+            List<int> targetA = new List<int>();
+            foreach (int item1 in roleA)
+            {
+                var xx = item1;
+                List<double> disA = new List<double>();
+                foreach (int item2 in roleB)
+                {
+                    //计算距离
+                    var dis = Math.Pow(Convert.ToInt32(arrA.GetValue(item1, posRow)) - Convert.ToInt32(arrB.GetValue(item2, posRow)), 2) + Math.Pow(Convert.ToInt32(arrA.GetValue(item1, posCol)) - Convert.ToInt32(arrB.GetValue(item2, posCol)), 2);
+                    disA.Add(dis);
+                }
+                //筛选出最小的，多个最小随机选取一个
+                var minv = disA.Min();
+                var minvIndex = disA.IndexOf(minv);
+                var mintemp = int.MaxValue;
+                List<int> minT = new List<int>();
+                List<int> minIN = new List<int>();
+                foreach (int i in disA)
+                {
+                    if (i < mintemp)
+                    {
+                        mintemp = i;
+                    }
+                }
+                foreach (int i in disA)
+                {
+                    var aa = 0;
+                    if (i == mintemp)
+                    {
+                        minIN.Add(aa);
+                    }
+                    aa++;
+                }
+                var lc = minIN.Count();
+                Random rndTar = new Random();
+                var rndSeed = rndTar.Next(lc);
+                var target = minIN[rndSeed];
+                var xxx = rndTar.Next(100);
+
+            }
+            //给B选择目标
+
         }
         //判断离自己最近位置的角色
-        public static void Distance(int roleNum1, int roleNum2)
-        { 
-
+        public static void Distance(object role1Row, object role1Col, object role2Row, object role2Col)
+        {
+            var dis = (Convert.ToInt32(role1Row) - Convert.ToInt32(role2Row)) + (Convert.ToInt32(role2Col) - Convert.ToInt32(role2Row));
         }
         //伤害计算逻辑
     }
