13758d9ca98aa85e478baffcbe67cdd55c344b3e
cent shen <360151507@qq.com>
2022-12-27 16:13:47 +0800

    beta1.0备份多线程返回数值

--- NumDesTools/DotaLegendBattle.cs
+++ NumDesTools/DotaLegendBattle.cs
@@ -633,4 +633,346 @@ namespace NumDesTools
             return data;
         }
     }
+    //多线程调用
+    //获取所有角色数据：默认和动态：：尝试多线程提高速度
+    //public static void getRoleData()
+    //{
+    //    //默认数据
+    //    //过滤空数据,A数据List化
+    //    var posRowA = DataList(groupARowNum, posRow, arrA, 1);
+    //    var nameA = NameList(groupARowNum, name, arrA, 1);
+    //    var posColA = DataList(groupARowNum, posCol, arrA, 1);
+    //    var posA = DataList(groupARowNum, pos, arrA, 1);
+    //    var atkA = DataList(groupARowNum, atk, arrA, 1);
+    //    var hpA = DataList(groupARowNum, hp, arrA, 1);
+    //    var hpAMax = DataList(groupARowNum, hp, arrA, 1);
+    //    var defA = DataList(groupARowNum, def, arrA, 1);
+    //    var critA = DataList(groupARowNum, crit, arrA, 1);
+    //    var critMultiA = DataList(groupARowNum, critMulti, arrA, 1);
+    //    var atkSpeedA = DataList(groupARowNum, atkSpeed, arrA, 1);
+    //    var skillCDA = DataList(groupARowNum, skillCD, arrA, 1);
+    //    var skillCDstartA = DataList(groupARowNum, skillCDstart, arrA, 1);
+    //    var skillDamgeA = DataList(groupARowNum, skillDamge, arrA, 1);
+    //    var skillHealUseSelfAtkA = DataList(groupARowNum, skillHealUseSelfAtk, arrA, 1);
+    //    var skillHealUseSelfHpA = DataList(groupARowNum, skillHealUseSelfHp, arrA, 1);
+    //    var skillHealUseAllHpA = DataList(groupARowNum, skillHealUseAllHp, arrA, 1);
+    //    var countATKA = DataList(groupARowNum, pos, arrA, 0); //普攻次数
+    //    var ATKDamageA = DataList(groupARowNum, pos, arrA, 2); //普攻威力
+    //    var countSkillA = DataList(groupARowNum, pos, arrA, 0);
+    //    var hpAddA = DataList(groupARowNum, pos, arrA, 0); //增血量
+    //    var hpSubA = DataList(groupARowNum, pos, arrA, 0); //减血量
+
+    //    //过滤空数据,B数据List化
+    //    var posRowB = DataList(groupARowNum, posRow, arrB, 1);
+    //    var nameB = NameList(groupARowNum, name, arrB, 1);
+    //    var posColB = DataList(groupARowNum, posCol, arrB, 1);
+    //    var posB = DataList(groupBRowNum, pos, arrB, 1);
+    //    var atkB = DataList(groupBRowNum, atk, arrB, 1);
+    //    var hpB = DataList(groupBRowNum, hp, arrB, 1);
+    //    var hpBMax = DataList(groupBRowNum, hp, arrB, 1);
+    //    var defB = DataList(groupBRowNum, def, arrB, 1);
+    //    var critB = DataList(groupBRowNum, crit, arrB, 1);
+    //    var critMultiB = DataList(groupBRowNum, critMulti, arrB, 1);
+    //    var atkSpeedB = DataList(groupBRowNum, atkSpeed, arrB, 1);
+    //    var skillCDB = DataList(groupBRowNum, skillCD, arrB, 1);
+    //    var skillCDstartB = DataList(groupBRowNum, skillCDstart, arrB, 1);
+    //    var skillDamgeB = DataList(groupBRowNum, skillDamge, arrB, 1);
+    //    var skillHealUseSelfAtkB = DataList(groupBRowNum, skillHealUseSelfAtk, arrB, 1);
+    //    var skillHealUseSelfHpB = DataList(groupBRowNum, skillHealUseSelfHp, arrB, 1);
+    //    var skillHealUseAllHpB = DataList(groupBRowNum, skillHealUseAllHp, arrB, 1);
+    //    var countATKB = DataList(groupARowNum, pos, arrB, 0); //普通次数
+    //    var ATKDamageB = DataList(groupARowNum, pos, arrA, 2); //普攻威力
+    //    var countSkillB = DataList(groupARowNum, pos, arrB, 0);
+    //    var hpAddB = DataList(groupARowNum, pos, arrA, 0); //增血量
+    //    var hpSubB = DataList(groupARowNum, pos, arrA, 0); //减血量
+    //    //每一轮数据变化：增量变化；当前数据+-=增量更新数据
+    //    int numA = hpA.Count;
+    //    int numB = hpB.Count;
+    //    int turn=0;
+    //    threadHelp refreshDataA = new threadHelp();
+    //    threadHelp refreshDataB = new threadHelp();
+    //    turnTime groundA = new turnTime();
+    //    turnTime groundB = new turnTime();
+    //    while (numA > 0 && numB > 0 && turn < 9001)
+    //    {
+    //        refreshDataA.countSkill1 = countSkillA;
+    //        refreshDataA.skillCD1 = skillCDA;
+    //        refreshDataA.skillCDstart1 = skillCDstartA;
+    //        refreshDataA.countATK1 = countATKA;
+    //        refreshDataA.atkSpeed1 = atkSpeedA;
+    //        refreshDataA.posRow1 = posRowA;
+    //        refreshDataA.posRow2 = posRowB;
+    //        refreshDataA.posCol1 = posColA;
+    //        refreshDataA.posCol2=posColB;
+    //        refreshDataA.def2 = defB;
+    //        refreshDataA.crit1 = critA;
+    //        refreshDataA.atk1 = atkA;
+    //        refreshDataA.critMulti1 = critMultiA;
+    //        refreshDataA.hpSub=hpSubA;
+    //        refreshDataA.skillDamge1=skillDamgeA;
+    //        refreshDataA.hp1 = hpA;
+    //        refreshDataA.hpAdd=hpAddA;
+    //        refreshDataA.skillHealUseAllHp1 = skillHealUseAllHpA;
+    //        refreshDataA.skillHealUseSelfAtk1=skillHealUseSelfAtkA;
+    //        refreshDataA.skillHealUseSelfHp1 = skillHealUseAllHpA;
+    //        refreshDataA.ATKDamage1 = ATKDamageA;
+    //        refreshDataA.num1 = numA;
+    //        refreshDataA.turn = turn;
+    //        refreshDataA.isAB = true;
+
+    //        refreshDataB.countSkill1 = countSkillB;
+    //        refreshDataB.skillCD1 = skillCDB;
+    //        refreshDataB.skillCDstart1 = skillCDstartB;
+    //        refreshDataB.countATK1 = countATKB;
+    //        refreshDataB.atkSpeed1 = atkSpeedB;
+    //        refreshDataB.posRow1 = posRowB;
+    //        refreshDataB.posRow2 = posRowA;
+    //        refreshDataB.posCol1 = posColB;
+    //        refreshDataB.posCol2 = posColA;
+    //        refreshDataB.def2 = defA;
+    //        refreshDataB.crit1 = critB;
+    //        refreshDataB.atk1 = atkB;
+    //        refreshDataB.critMulti1 = critMultiB;
+    //        refreshDataB.hpSub = hpSubB;
+    //        refreshDataB.skillDamge1 = skillDamgeB;
+    //        refreshDataB.hp1 = hpB;
+    //        refreshDataB.hpAdd = hpAddB;
+    //        refreshDataB.skillHealUseAllHp1 = skillHealUseAllHpB;
+    //        refreshDataB.skillHealUseSelfAtk1 = skillHealUseSelfAtkB;
+    //        refreshDataB.skillHealUseSelfHp1 = skillHealUseAllHpB;
+    //        refreshDataB.ATKDamage1 = ATKDamageB;
+    //        refreshDataB.num1 = numB;
+    //        refreshDataB.turn = turn;
+    //        refreshDataB.isAB = false;
+
+    //        numA = hpA.Count;
+    //        numB = hpB.Count;
+
+    //        //A、B同步进行战斗A
+    //        Thread threadA = new Thread(groundA.battleData);
+    //        threadA.Name = " A攻击 ";
+    //        threadA.Start(refreshDataA);
+    //        hpAddA = refreshDataA.hpAdd;
+    //        hpSubA = refreshDataA.hpSub;
+    //        countSkillA = refreshDataA.countSkill1;
+    //        countATKA = refreshDataA.countATK1;
+    //        Debug.Print(threadA.Name+"A线程执行完毕");
+
+    //        Thread threadB = new Thread(groundB.battleData);
+    //        threadB.Name = " B攻击 ";
+    //        threadB.Start(refreshDataB);
+    //        hpAddB = refreshDataA.hpAdd;
+    //        hpSubB = refreshDataA.hpSub;
+    //        countSkillB = refreshDataA.countSkill1;
+    //        countATKB = refreshDataA.countATK1;
+    //        Debug.Print(threadB.Name + "B线程执行完毕");
+
+    //        //同步数据
+    //        var cac = hpAddA.Count;
+    //        var nu123 = numA;
+    //        var asdd = turn;
+    //        var asd=0;
+    //        for (int i = 0; i < numA; i++)
+    //        {
+    //            hpA[i] += hpAddA[i];
+    //            hpA[i] = Math.Min(hpA[i], hpAMax[i]);
+    //            hpA[i] -= hpSubB[i];
+    //            hpA[i] = Math.Min(hpA[i], hpAMax[i]);
+    //            hpAddA[i] = 0;
+    //            hpSubB[i] = 0;
+    //            if (hpA[i] <= 0)
+    //            {
+    //                posRowA.RemoveAt(i);
+    //                posColA.RemoveAt(i);
+    //                hpA.RemoveAt(i);
+    //                defA.RemoveAt(i);
+    //                //posA.RemoveAt(i);
+    //                atkA.RemoveAt(i);
+    //                critA.RemoveAt(i);
+    //                critMultiA.RemoveAt(i);
+    //                atkSpeedA.RemoveAt(i);
+    //                skillCDA.RemoveAt(i);
+    //                skillCDstartA.RemoveAt(i);
+    //                skillDamgeA.RemoveAt(i);
+    //                skillHealUseSelfAtkA.RemoveAt(i);
+    //                skillHealUseSelfHpA.RemoveAt(i);
+    //                skillHealUseAllHpA.RemoveAt(i);
+    //                countSkillA.RemoveAt(i);
+    //                countATKA.RemoveAt(i);
+    //                hpAMax.RemoveAt(i);
+    //                hpAddA.RemoveAt(i);
+    //                hpSubA.RemoveAt(i);
+    //            }
+    //        }
+    //        for (int i = 0; i < numB; i++)
+    //        {
+    //            hpB[i] += hpAddB[i];
+    //            hpB[i] = Math.Min(hpB[i], hpBMax[i]);
+    //            hpB[i] -= hpSubA[i];
+    //            hpB[i] = Math.Min(hpB[i], hpBMax[i]);
+    //            hpAddB[i] = 0;
+    //            hpSubA[i] = 0;
+    //            if (hpB[i] <= 0)
+    //            {
+    //                posRowB.RemoveAt(i);
+    //                posColB.RemoveAt(i);
+    //                hpB.RemoveAt(i);
+    //                defB.RemoveAt(i);
+    //                //posB.RemoveAt(i);
+    //                atkB.RemoveAt(i);
+    //                critB.RemoveAt(i);
+    //                critMultiB.RemoveAt(i);
+    //                atkSpeedB.RemoveAt(i);
+    //                skillCDB.RemoveAt(i);
+    //                skillCDstartB.RemoveAt(i);
+    //                skillDamgeB.RemoveAt(i);
+    //                skillHealUseSelfAtkB.RemoveAt(i);
+    //                skillHealUseSelfHpB.RemoveAt(i);
+    //                skillHealUseAllHpB.RemoveAt(i);
+    //                countSkillB.RemoveAt(i);
+    //                countATKB.RemoveAt(i);
+    //                hpBMax.RemoveAt(i);
+    //                hpAddB.RemoveAt(i);
+    //                hpSubB.RemoveAt(i);
+    //            }
+    //        }
+    //        turn++;
+    //    }
+
+    //    var aaa = numA;
+    //    var bbb = numB;
+
+    //}
+    //参数类
+    //class threadHelp
+    //{
+    //    public List<double> countSkill1,
+    //        skillCD1,
+    //        skillCDstart1,
+    //        countATK1,
+    //        atkSpeed1,
+    //        posRow1,
+    //        posRow2,
+    //        posCol1,
+    //        posCol2,
+    //        def2,
+    //        crit1,
+    //        atk1,
+    //        critMulti1,
+    //        hpSub,
+    //        skillDamge1,
+    //        hp1,
+    //        hpAdd,
+    //        skillHealUseAllHp1,
+    //        skillHealUseSelfAtk1,
+    //        skillHealUseSelfHp1,
+    //        ATKDamage1;
+    //    //dynamic countSkill1, dynamic countATK1, dynamic atkSpeed1, dynamic turn, dynamic skillCD1, dynamic skillCDstart1, dynamic num1,
+    //    //    dynamic posRow1, dynamic posRow2, dynamic posCol1, dynamic posCol2, dynamic def2, dynamic crit1, dynamic atk1, dynamic critMulti1, dynamic skillDamge1
+    //    //, dynamic hp2, dynamic name1, dynamic name2, dynamic hp1, dynamic skillHealUseAllHp1, dynamic skillHealUseSelfAtk1, dynamic skillHealUseSelfHp1, dynamic isAB
+    //    //, dynamic hpAdd, dynamic hpSub, dynamic ATKDamage1
+    //    public int num1;
+    //    public int turn;
+    //    public bool isAB;
+    //}
+    //核心计算函数
+    //class turnTime
+    //{
+    //    public void battleData(object refreshDataOBJ)
+    //    {
+    //        List<double> countSkill1 =(refreshDataOBJ as threadHelp) .countSkill1;
+    //        List<double> skillCD1 = (refreshDataOBJ as threadHelp).skillCD1;
+    //        List<double> skillCDstart1 = (refreshDataOBJ as threadHelp).skillCDstart1;
+    //        List<double> countATK1 = (refreshDataOBJ as threadHelp).countATK1;
+    //        List<double> atkSpeed1 = (refreshDataOBJ as threadHelp).atkSpeed1;
+    //        List<double> posRow2 = (refreshDataOBJ as threadHelp).posRow2;
+    //        List<double> posCol1 = (refreshDataOBJ as threadHelp).posCol1;
+    //        List<double> posCol2 = (refreshDataOBJ as threadHelp).posCol2;
+    //        List<double> def2 = (refreshDataOBJ as threadHelp).def2;
+    //        List<double> crit1 = (refreshDataOBJ as threadHelp).crit1;
+    //        List<double> atk1 = (refreshDataOBJ as threadHelp).atk1;
+    //        List<double> critMulti1 = (refreshDataOBJ as threadHelp).critMulti1;
+    //        List<double> hpSub = (refreshDataOBJ as threadHelp).hpSub;
+    //        List<double> skillDamge1 = (refreshDataOBJ as threadHelp).skillDamge1;
+    //        List<double> posRow1 = (refreshDataOBJ as threadHelp).posRow1;
+    //        List<double> hpAdd = (refreshDataOBJ as threadHelp).hpAdd;
+    //        List<double> skillHealUseAllHp1 = (refreshDataOBJ as threadHelp).skillHealUseAllHp1;
+    //        List<double> skillHealUseSelfAtk1 = (refreshDataOBJ as threadHelp).skillHealUseSelfAtk1;
+    //        List<double> skillHealUseSelfHp1 = (refreshDataOBJ as threadHelp).skillHealUseSelfHp1;
+    //        List<double> ATKDamage1 = (refreshDataOBJ as threadHelp).ATKDamage1;
+    //        List<double> hp1 = (refreshDataOBJ as threadHelp).hp1;
+    //        int turn = (refreshDataOBJ as threadHelp).turn;
+    //        int num1 = (refreshDataOBJ as threadHelp).num1;
+    //        bool isAB = (refreshDataOBJ as threadHelp).isAB;
+    //        for (int i = 0; i < num1; i++)
+    //        {
+    //            //判断释放技能类型、ID//战斗计算，攻速和CD放大100倍进行判定
+    //            var aa = countSkill1[i] * Convert.ToInt32(skillCD1[i] * 1);
+    //            var bb = Convert.ToInt32(skillCDstart1[i] * 1);
+    //            var cc = countATK1[i] * Convert.ToInt32(1 / atkSpeed1[i] * 1);
+    //            var isSkill = 0;
+    //            if (aa + bb == turn) //判断技能CD
+    //                isSkill = 1;
+    //            else if (cc == turn) //判断普攻CD（攻速）
+    //                isSkill = 0;
+    //            else
+    //                isSkill = -1; //不攻击
+    //            //判断目标
+    //            var target = DotaLegendBattle.Target(i, posRow1, posRow2, posCol1, posCol2);
+    //            //计算技能伤害、治疗效果、Buff等(汇总增量)
+    //            Random rndCrit = new Random();
+    //            var rSeed = rndCrit.Next(10000);
+    //            double dmg = 0;
+    //            double redmg = def2[target] / 100000 + 1;
+    //            if (Convert.ToInt32(crit1[i] * 10000) >= rSeed)
+    //            {
+    //                dmg = atk1[i] * critMulti1[i];
+    //            }
+    //            else
+    //            {
+    //                dmg = atk1[i];
+    //            }
+    //            var tempRole1 = "";
+    //            if (tempRole1 == null) throw new ArgumentNullException(nameof(tempRole1));
+    //            var tempRole2 = "";
+    //            if (tempRole2 == null) throw new ArgumentNullException(nameof(tempRole2));
+    //            if (isAB)
+    //            {
+    //                tempRole1 = "A组";
+    //                tempRole2 = "B组";
+    //            }
+    //            else
+    //            {
+    //                tempRole1 = "B组";
+    //                tempRole2 = "A组";
+    //            }
+    //            if (isSkill == 1)
+    //            {
+    //                //目标减血
+    //                hpSub[target] = dmg / redmg * skillDamge1[i];
+    //                //battleLog += name1[i] + "[" + tempRole1 + "]" + "技能攻击" + name2[target] + "[" + tempRole2 + "]" + "造成伤害：" + Convert.ToInt32(hpSub[target]) + "\r\n";
+    //                //自身加血
+    //                for (int j = 0; j < hp1.Count; j++)
+    //                {
+    //                    hpAdd[j] = skillHealUseAllHp1[i] * hp1[j];
+    //                    //battleLog += name1[i] + "[" + tempRole1 + "]" + "治疗" + name1[j] + "[" + tempRole1 + "]" + "回复血量：" + Convert.ToInt32(hpAdd[j]) + "\r\n";
+    //                }
+    //                hpAdd[i] = skillHealUseSelfAtk1[i] * atk1[i] + skillHealUseSelfHp1[i] * hp1[i];
+    //                //battleLog += name1[i] + "[" + tempRole1 + "]" + "治疗自己，回复血量：" + Convert.ToInt32(skillHealUseSelfAtk1[i] * atk1[i] + skillHealUseSelfHp1[i] * hpAdd[i]) + "\r\n";
+    //                countSkill1[i]++;
+    //            }
+    //            else if (isSkill == 0)
+    //            {
+    //                //目标减血
+    //                hpSub[target] = dmg / redmg * ATKDamage1[i];
+    //                //battleLog += name1[i] + "[" + tempRole1 + "]" + "普通攻击" + name2[target] + "[" + tempRole2 + "]" + "造成伤害：" + Convert.ToInt32(hpSub[target]) + "\r\n";
+    //                countATK1[i]++;
+    //            }
+    //        }
+    //        (refreshDataOBJ as threadHelp).hpSub = hpSub;
+    //        (refreshDataOBJ as threadHelp).hpAdd = hpAdd;
+    //        (refreshDataOBJ as threadHelp).countSkill1 = countSkill1;
+    //        (refreshDataOBJ as threadHelp).countATK1 = countATK1;
+    //    }
+    //}
 }
