09c636e829784e886bad9b869e060fa3f387a0f5
cent shen <360151507@qq.com>
2023-01-10 15:35:18 +0800

    还原数据

--- NumDesTools/CellSelectChangePro.cs
+++ NumDesTools/CellSelectChangePro.cs
@@ -1,38 +1,28 @@
 ﻿using ExcelDna.Integration;
 using Microsoft.Office.Interop.Excel;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace NumDesTools
 {
-    public class class1
+    public class CellSelectChangePro
     {
         private static readonly dynamic app = ExcelDnaUtil.Application;
-        private static readonly Workbook wk = app.Workbooks["角色怪物数据生成"];
-        private static readonly Worksheet ws = wk.Worksheets["角色基础"];
-        public void CellSelectChangePro()
+        public event Microsoft.Office.Interop.Excel.DocEvents_BeforeDoubleClickEventHandler BeforeDoubleClick;
+        public   CellSelectChangePro()
         {
-            //单表选择单元格触发
-            ws.SelectionChange += new Excel.DocEvents_SelectionChangeEventHandler(GetCellValue);
-            //全（多）工作簿选择单元格触发
-            //app.SheetSelectionChange += new Excel.WorkbookEvents_SheetSelectionChangeEventHandler(getCellValue);;
+            this.BeforeDoubleClick += new DocEvents_BeforeDoubleClickEventHandler(Worksheet1_BeforeDoubleClick);
         }
 
-        public void GetCellValue(Excel.Range target)
+        public void Worksheet1_BeforeDoubleClick(Excel.Range Target,
+            ref bool Cancel)
         {
-            var cRow = target.Row;
-            var cCol = target.Column;
-            if (cRow<16&&cCol<5||cCol>18) return;
-            ws.Range["U1"].Value2 = ws.Range[ws.Cells[cRow,5]].Value2;
-        }
-        public void getCellValue(object Sh ,Excel.Range target)
-        {
-            var cRow = target.Row;
-            var cCol = target.Column;
-            if (cRow < 16 && cCol < 5 || cCol > 18) return;
-            ws.Range["U1"].Value2 = ws.Range[ws.Cells[cRow, 5]].Value2;
+            MessageBox.Show("Double-clicking in this sheet" + " is not allowed.");
+            Debug.Print("tesd1");
+            Cancel = true;
         }
     }
 }
