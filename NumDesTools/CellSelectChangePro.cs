2b7c0f4630a8df9132e10191d7eda0979fac92d4
cent shen <360151507@qq.com>
2023-01-11 10:38:37 +0800

    ribbon中控件label内容更改的问题解决了

--- NumDesTools/CellSelectChangePro.cs
+++ NumDesTools/CellSelectChangePro.cs
@@ -11,9 +11,10 @@ namespace NumDesTools
         public CellSelectChangePro()
         {
             //单表选择单元格触发
-            ws.SelectionChange += GetCellValue;
+            //ws.SelectionChange += GetCellValue;
+            //多表选择单元格触发
+            app.SheetSelectionChange += new Excel.WorkbookEvents_SheetSelectionChangeEventHandler(getCellValue);
         }
-
         public void GetCellValue(Excel.Range range)
         {
             if (CreatRibbon.LabelTextRoleDataPreview == "角色数据预览：开启")
@@ -37,6 +38,43 @@ namespace NumDesTools
                         //MessageBox.Show("没有找到角色数据");
                     }
                 }
+            }
+        }
+        public void getCellValue(object sh,Excel.Range range)
+        {
+            Excel.Worksheet ws2 = app.ActiveSheet;
+            var name = ws2.Name;
+            if(name == "角色基础")
+            {
+                if (CreatRibbon.LabelTextRoleDataPreview == "角色数据预览：开启")
+                {
+                    if (range.Row < 16 || range.Column < 5 || range.Column > 18)
+                    {
+                        app.StatusBar = "当前行不是角色数据行，另选一行";
+                        //MessageBox.Show("单元格越界");
+                    }
+                    else
+                    {
+                        var roleName = ws2.Cells[range.Row, 5].Value2;
+                        if (roleName != null)
+                        {
+                            ws2.Range["U1"].Value2 = roleName;
+                            app.StatusBar = "角色：【" + roleName + "】数据已经更新，右侧查看~！~→→→→→→→→→→→→→→→~！~";
+                        }
+                        else
+                        {
+                            app.StatusBar = "当前行没有角色数据，另选一行";
+                            //MessageBox.Show("没有找到角色数据");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                CreatRibbon.LabelTextRoleDataPreview = "角色数据预览：关闭";
+                //更新控件lable信息
+                CreatRibbon.R.InvalidateControl("Button14");
+                app.StatusBar = "当前非【角色基础】表，数据预览功能关闭";
             }
         }
     }
