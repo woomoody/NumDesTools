bd714889d36e4b487400245357528ed7fcdf69b8
cent <cent@newborntown.com>
2023-02-22 19:14:36 +0800

    打包问题解决：打包只能用1.1版本的模式。1.6的不行

--- pack/packtool.bat
+++ pack/packtool.bat
@@ -1,5 +1,6 @@
 set packpath=%1%
 
-%packpath%ExcelDnaPack %packpath%NumDesTools.dna /Y /O %packpath%NumDesToolsPack.XLL
-%packpath%ExcelDnaPack %packpath%NumDesTools64.dna /Y /O %packpath%NumDesToolsPack64.XLL
+%packpath%ExcelDnaPack %packpath%NumDesTools-AddIn64.dna /Y /O %packpath%NumDesToolsPack64.XLL
+%packpath%ExcelDnaPack %packpath%NumDesTools-AddIn.dna /Y /O %packpath%NumDesToolsPack.XLL
+
 pause
