f771167f9851fa78890288dea0520ed35d841fc4
cent <cent@newborntown.com>
2023-04-28 15:47:27 +0800

    dot -Tpng -o tree.png output.dot graphviz这个工具好玩

---
+++ NumDesTools/ExcelLinksGraphic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using ExcelDna.Integration;
+
+namespace NumDesTools;
+
+public class ExcelLinksGraphic
+{
+    private static readonly dynamic App = ExcelDnaUtil.Application;
+    public static void Graph()
+    {
+        //打开Excel文件
+        var workbook = App.ActiveWorkbook;
+        var sheet = workbook.ActiveSheet;
+        //读取Excel文件统计数据
+        Dictionary<string, List<string>> mainExcel = new Dictionary<string, List<string>>();
+        var usedRange = sheet.UsedRange;
+        for (int row = 1; row <= usedRange.Rows.Count; row++)
+        {
+            List<string> linkExcel = new List<string>();
+            var mainCell = usedRange.Cells[row, 1].Value;
+            string mainValue = "";
+            if (mainCell != null)
+            {
+                mainValue = mainCell.ToString();
+            }
+            if (mainValue!= "")
+            {
+                for (int col = 2; col <= usedRange.Columns.Count; col++)
+                {
+                    var linkCell = usedRange.Cells[row, col].Value;
+                    string linkValue = "";
+                    if (linkCell != null)
+                    {
+                        linkValue = linkCell.ToString() + @".xlsx";
+                    }
+                    if (linkValue != "")
+                    {
+                        linkExcel.Add(linkValue);
+                    }
+                }
+                mainExcel[mainValue] = linkExcel;
+            }
+        }
+        //生成关系图
+        using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\admin\Desktop\output.dot"))
+        {
+            file.WriteLine("digraph G {");
+            foreach (KeyValuePair<string, List<string>> pair in mainExcel)
+            {
+                foreach (string field in pair.Value)
+                {
+                    if (mainExcel.ContainsKey(field))
+                    {
+                        file.WriteLine("\"" + pair.Key + "\" -> \"" + field + "\"");
+                    }
+                }
+            }
+            file.WriteLine("}");
+        }
+    }
+}
