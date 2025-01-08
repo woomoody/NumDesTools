1a7b033eebc42371521c00a20f1fc2b06a86f4c5
cent <cent@solotopiax.com>
2025-01-08 11:12:11 +0800

    top_p不能设置5

--- NumDesTools/UI/AIChatTaskPanel.xaml.cs
+++ NumDesTools/UI/AIChatTaskPanel.xaml.cs
@@ -222,6 +222,9 @@ namespace NumDesTools.UI
                     new { role = "user", content = userInput }
                 },
                 max_tokens = 10000,     // 最大生成的 token 数量
+                temperature = 0.5,     // 控制生成的随机性
+                top_p = 1,             // 核采样参数
+                stream = false          // 启用流式输出
             };
         }
 
