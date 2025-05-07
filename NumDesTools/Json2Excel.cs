23681f54bb75bef9deb691b25062f65310f5c577
cent <cent@solotopiax.com>
2025-05-07 17:40:33 +0800

    reaharp var化

--- NumDesTools/Json2Excel.cs
+++ NumDesTools/Json2Excel.cs
@@ -5,7 +5,7 @@ namespace NumDesTools;
 
 public class JsonToExcelConverter
 {
-    public void ConvertMultipleJsonToExcel(string jsonFilePath )
+    public void ConvertMultipleJsonToExcel(string jsonFilePath)
     {
         // 收集所有可能的键（列标题）
         var allKeys = new HashSet<string>();
@@ -13,34 +13,31 @@ public class JsonToExcelConverter
 
         // 解析所有JSON文件
 
-        string jsonContent = File.ReadAllText(jsonFilePath);
+        var jsonContent = File.ReadAllText(jsonFilePath);
 
         // 修改后：先解析为对象，再提取"list"数组
         var jsonObject = JObject.Parse(jsonContent);
         var jsonArray = (JArray)jsonObject["list"]; // 显式转换为JArray
 
         if (jsonObject["list"] == null || jsonObject["list"].Type != JTokenType.Array)
-        {
             throw new InvalidDataException("JSON中缺少有效的list数组");
-        }
 
         Debug.Assert(jsonArray != null, nameof(jsonArray) + " != null");
         foreach (var item in jsonArray)
         {
             var rowDict = new Dictionary<string, object>();
-            foreach (JProperty prop in item.Children<JProperty>())
-            {
+            foreach (var prop in item.Children<JProperty>())
                 // 仅处理简单值，忽略嵌套对象或数组
                 if (prop.Value.Type != JTokenType.Object && prop.Value.Type != JTokenType.Array)
                 {
-                    string key = prop.Name;
+                    var key = prop.Name;
                     allKeys.Add(key);
                     rowDict[key] = prop.Value.ToString();
                 }
-            }
+
             allRows.Add(rowDict);
         }
-        
+
         var excelFilePath = Path.ChangeExtension(jsonFilePath, ".xlsx");
 
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
@@ -50,7 +47,7 @@ public class JsonToExcelConverter
         var worksheet = package.Workbook.Worksheets.Add("Data");
 
         // 写入列标题（动态处理所有可能的键）
-        int col = 1;
+        var col = 1;
         foreach (var key in allKeys)
         {
             worksheet.Cells[1, col].Value = key;
@@ -58,18 +55,16 @@ public class JsonToExcelConverter
         }
 
         // 填充数据行
-        int row = 2;
+        var row = 2;
         foreach (var rowDict in allRows)
         {
             col = 1;
             foreach (var key in allKeys)
             {
-                if (rowDict.TryGetValue(key, out object value))
-                {
-                    worksheet.Cells[row, col].Value = value;
-                }
+                if (rowDict.TryGetValue(key, out var value)) worksheet.Cells[row, col].Value = value;
                 col++;
             }
+
             row++;
         }
 
